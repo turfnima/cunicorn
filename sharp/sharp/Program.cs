cd545ea7f5393280c96307da7eff299421d5bd22
turfnima <lxzno3@gmail.com>
2020-04-22 08:14:41 -0700

    practice

---
+++ sharp/sharp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sharp
+{
+    public class Person
+    {
+        public string Name;
+        
+        public void Introduce(string to)
+        {
+            Console.WriteLine("Hi {0} , I am {1}", to, Name);
+        }
+
+        public static Person Parse(string str)
+        {
+            var person = new Person();
+            person.Name = str;
+            return person;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var person = Person.Parse("Jhon");
+            
+            person.Introduce("Mosh");
+
+        }
+    }
+}
